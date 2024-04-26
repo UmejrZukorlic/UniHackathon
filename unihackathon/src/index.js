82b8b81d3193be54cbb16fc7e32244bd3bcc974a
Umejr Zukorlic <89653651+UmejrZukorlic@users.noreply.github.com>
2024-04-27 01:37:40 +0200

    sada

--- unihackathon/src/index.js
+++ unihackathon/src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import CourseSelector from "./components/courses/CourseSelector";
-import Main from "./components/Main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Main />
     <CourseSelector />
   </React.StrictMode>
 );
